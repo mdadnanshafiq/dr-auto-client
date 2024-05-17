f0c194bba7179b46e443e9a6e1f5ad9fdf89c7b1
mdadnanshafiq <adnanbgic2023@gmail.com>
2024-05-18 04:43:21 +0600

    search and sort added

--- src/Pages/Home/Services.jsx
+++ src/Pages/Home/Services.jsx
@@ -3,11 +3,22 @@ import ServiceCard from "./ServiceCard";
 
 const Services = () => {
   const [services, setServices] = useState([]);
+  const [asc, setAsc] = useState(true);
+  const [search, setSearch] = useState("");
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const searchText = e.target.search.value;
+    setSearch(searchText);
+  };
   useEffect(() => {
-    fetch("https://dr-auto-server-wine.vercel.app/services")
+    fetch(
+      `https://dr-auto-server-wine.vercel.app/services?sort=${
+        asc ? "asc" : "desc"
+      }&search=${search}`
+    )
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, []);
+  }, [asc, search]);
   return (
     <div className="w-full mx-auto">
       <div className="flex flex-col justify-center items-center gap-4">
@@ -17,6 +28,21 @@ const Services = () => {
           The majority have suffered alteration in some form, by injected
           humour, or randomised words which dont look even slightly believable.{" "}
         </p>
+        <form onSubmit={handleSearch}>
+          <input
+            name="search"
+            type="text"
+            className="input input-bordered join join-item"
+          />
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-ghost join-item join"
+          />
+        </form>
+        <button onClick={() => setAsc(!asc)} className="btn btn-outline">
+          {asc ? "Price: High to Low" : "Price: Low to High"}
+        </button>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-center items-center w-full mx-auto">
         {services.map((service, idx) => (
